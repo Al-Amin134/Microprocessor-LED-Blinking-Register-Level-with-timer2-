4578e5d232f33ca8fba18c0165f9d2b09ea75af8
Md.Al-Amin Babu <mdalaminbabu.210@gmail.com>
2025-05-19 01:41:56 +0600

    using timer1.c

--- tst1/using timer1.c
+++ tst1/using timer1.c
@@ -1,5 +1,5 @@
-#define RCC_BASE  		      0x40021000   
-#define GPIOC_BASE 		      0x40011000    
+#define RCC_BASE  		  0x40021000   
+#define GPIOC_BASE 		  0x40011000    
 
 #define RCC_APB2ENR    		 (*(volatile unsigned int *)(RCC_BASE + 0x18))
 #define GPIOC_CRH     		 (*(volatile unsigned int *)(GPIOC_BASE + 0x04))
