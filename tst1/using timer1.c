c45e90efca9a9bad2248fc98e5b1f226d9ede333
Md.Al-Amin Babu <mdalaminbabu.210@gmail.com>
2025-05-19 19:07:07 +0600

    GPIOA and TIM1.c

--- tst1/using timer1.c
+++ tst1/using timer1.c
@@ -1,40 +1,39 @@
-#define RCC_BASE  		  0x40021000   
-#define GPIOC_BASE 		  0x40011000    
-
-#define RCC_APB2ENR    		 (*(volatile unsigned int *)(RCC_BASE + 0x18))
-#define GPIOC_CRH     		 (*(volatile unsigned int *)(GPIOC_BASE + 0x04))
-#define GPIOC_ODR    		 (*(volatile unsigned int *)(GPIOC_BASE + 0x0c))
-#define GPIOC_BSRR 		 *(volatile unsigned int *) (GPIOC_BASE + 0x10)
-
+#define RCC_BASE 0x40021000
+#define GPIOA 0x40010800
+#define DELAY 1000000
+#define RCC_APB2ENR *(volatile unsigned int *)(RCC_BASE+0X18)
+#define GPIOA_CRH *(volatile unsigned int *) (GPIOA + 0X04)
+#define GPIOA_BSRR *(volatile unsigned int *) (GPIOA + 0X10)
+	
+//BY USING TIMER1
 #define TIM1 0x40012C00
-#define TIM1_CR1         	 *(volatile unsigned int *)(TIM1 + 0x00)
-#define TIM1_PSC         	 *(volatile unsigned int *)(TIM1 + 0x28)
-#define TIM1_ARR       		 *(volatile unsigned int *)(TIM1 + 0x2c)
-#define TIM1_SR           	  *(volatile unsigned int *)(TIM1 + 0x10)
-			
-
-
+#define TIM1_CR1 *(volatile unsigned int*)(TIM1+0X00)
+#define TIM1_PSC *(volatile unsigned int*)(TIM1+0X28)
+#define TIM1_ARR *(volatile unsigned int*)(TIM1+0X2C)
+#define TIM1_SR *(volatile unsigned int*)(TIM1+0X10)
+#define GPIOA_ODR *(volatile unsigned int*)(GPIOA+0X0C)
 int main()
 {
-	 RCC_APB2ENR |=(1u<<4);
-	 GPIOC_CRH &=~(0xF<<20);
-	 GPIOC_CRH &=~(1u<<20);
-	 GPIOC_CRH |=(1u<<21);
-	 GPIOC_CRH &=~(1u<<22);
-	 GPIOC_CRH &=~(1u<<23);
+	RCC_APB2ENR |=(1U<<20);
+	GPIOA_CRH &=~(0XF<<20);
+	GPIOA_CRH &=~(1U<<20);
+	GPIOA_CRH |=(1U<<21);
+	GPIOA_CRH	&=~(1U<<22);
+	GPIOA_CRH &=~(1U<<23);
 	
-	RCC_APB2ENR |=(1u<<11);
-	
-	TIM1_CR1  |=(1u<<0);
-	TIM1_ARR = 1000;
+	RCC_APB2ENR |=(1U<<11);
 	TIM1_PSC = 7999;
+	TIM1_ARR = 999;
+	TIM1_CR1 |=(1U<<0);
 	
-	 while(1)
-	 {
-		if(TIM1_SR&(1u<<0))
-		 {
-			TIM1_SR &=~(1u<<0);
-			GPIOC_ODR ^=(1u<<13);
+	while(1)
+	{
+		if(TIM1_SR & (1U<<0)){
+			TIM1_SR &=~(1U<<0);
+			GPIOA_ODR^=(1U<<13);
+			
 		}
-	 }
+		
+	}
+	
 }
